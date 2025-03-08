78c6221369e50d61101cd6ed764b6593f8e404c9
nefims <nefims@gmail.com>
2025-03-08 03:53:02 -0300

    Port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongodb = require('./data/database');
 const app = express();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 10000;
 
 app.use('/', require('./routes'));
 
